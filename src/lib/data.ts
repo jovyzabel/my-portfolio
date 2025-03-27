299825ce22f2484ace43e49902d16c05c86b34bb
jovyzabel <jovymonka7@gmail.com>
2025-03-27 23:56:57 +0100

    Jovy M astro portfolio

---
+++ src/lib/data.ts
@@ -0,0 +1,230 @@
+export const personalInfo = {
+  name: "Jovy Nedved Monka - K",
+  location: "Brazzaville, Congo",
+  email: "jovymonka7@gmail.com",
+  github: "https://github.com/jovyzabel",
+  linkedin: "https://www.linkedin.com/in/jovymonka/",
+};
+
+export const workExperience = [
+  {
+    company: "FST, UMNG",
+    location: "Brazzaville, Congo",
+    position: "Web Developer",
+    period: "Nov 2022 - June 2024",
+    achievements: [
+      "Development of an application for managing the schooling and teaching of the the Faculty of Science and Technology at Marien Ngouabi University as part of the AMES project, financed by AFD.",
+      // "Designed and implemented business logic for AWS Lambdas using the Serverless Framework, forming the backbone of the application's functionality and ensuring seamless backend operations.",
+      // "Built a comprehensive admin panel for managing content, user data, and analytics.",
+      // "Contributed to the development and integration of a scalable CMS for managing blog content and other platform data.",
+      // "Reduced database costs by introducing materialized views, optimizing query performance and resource usage.",
+      // "Drove the integration of a custom affiliate marketing system, enabling seamless tracking and reporting of referrals and user activities.",
+      // "Integrated Text-to-Speech (TTS) services for enhanced user experiences, optimizing both frontend and backend systems.",
+      // "Collaborated with cross-functional teams to deliver high-quality features, actively reviewing pull requests to ensure code quality, adherence to standards, and efficient implementation.",
+    ],
+  },
+  {
+    company: "S2DEC",
+    location: "Brazzaville, Congo",
+    position: "IT Support Specialist",
+    period: "2023 - 2024",
+    achievements: [
+      "Work as a IT service provider for the company.",
+      "Ensure security of data and network access",
+    ],
+  },
+  {
+    company: "PressiRessort",
+    location: "Remote",
+    position: "Web Developer Intern",
+    period: "Dec 2021 - Mar 2022",
+    achievements: [
+      "Built An app for Buying torsion spring and tension spring",
+    ],
+  },
+];
+
+export const education = [
+  {
+    institution: "OBJIS",
+    location: "Remote, Lyon, France",
+    degree: "Training in Java And Angular",
+    period: "Jan 2024 - Aug 2025",
+    achievements: [
+      "Front end applications development",   
+    ],
+  },
+  {
+    institution: "Akieni Academy",
+    location: "Brazzaville, Congo",
+    degree: "Js Fullstack Developer",
+    period: "Nov 2024 - Jun 2025",
+    achievements: [
+      "Front end applications development",   
+    ],
+  },
+  {
+    institution: "Faculty of Science - MARIEN NGOUABI University",
+    location: "Brazzaville, Congo",
+    degree: "B Computer Science",
+    period: "Jun 2019 - Jun 2021",
+    achievements: [
+      "Team leader for classworks",   
+    ],
+  },
+  {
+    institution: "EAMAC - African Civil Aviation and Meteorological School",
+    location: "Niamey, Niger",
+    degree: "Superior Technician in Telecommunications exploitation",
+    period: "Jun 2017 - Jun 2019",
+    achievements: [
+      "Captain of a class football team",   
+    ],
+  },
+];
+export const skills = {
+  programmingLanguages: [
+    "Java",
+    "TypeScript",
+    "JavaScript",
+    "Php",    
+    "Sql",
+  ],
+  frontendDevelopment: [
+    "Angular 16+",
+    "Bootstrap CSS",
+    "HTML",
+    "CSS",
+  ],
+  backendDevelopment: ["Symfony 6+"],
+  databaseAndStorage: ["MySQL", "Doctrine (ORM)"],
+  cloudAndDevOps: ["Docker", "Git"],
+  toolsAndServices: [
+    "Ngrok",
+    "Jira",
+    // "Clerk (Auth)",
+    // "Sanity (CMS)",
+    // "Tinybird (analytics)",
+    // "Zod",
+    // "Sentry",
+    // "Mixpanel",
+    // "Trigger.dev",
+  ],
+};
+
+export const projects = [
+  {
+    title: "Snapface",
+    github: "https://github.com/jovyzabel/snapface",
+    description: [
+      "An web app for sharing photos with friends and family.",
+      "Built with Angular 18",
+      "Usage of standalone components",
+      // "WiFi-RTT: Developed indoor occupancy tracking for energy optimization.",
+      // "IoT Solutions: Implemented smart monitoring for energy efficiency and food waste reduction.",
+      // "Real-Time Tracking: Designed systems to monitor carbon emissions and optimize resources.",
+      // "Data-Driven Insights: Analyzed user patterns for adaptive energy and food management.",
+      // "Reward Integration: Built QR-based green points system to incentivize eco-friendly actions.",
+    ],
+  },
+  {
+    title: "Todo list",
+    github: "https://github.com/jovyzabel/todo-list",
+    description: [
+      "A simple todo list app.",
+      "Built with JavaScript, HTML, and CSS.",
+      // "A holistic mental health app leveraging AI and blockchain for early detection and personalized solutions for depression.",
+      // "Features include social media analysis (ML-based sentiment tracking), PHQ-9 assessments, smartwatch integration for sleep and activity data, guided meditation, and smart suggestions for food, travel, music, and movies.",
+      // "Developed a rewards system using MAG crypto token on Ethereum, enabling user engagement through in-app incentives.",
+      // "Secure storage implemented via web3.storage, IPFS, and FileCoin.",
+      // "Built with React, Supabase, Node.js, Flask, and Solidity, showcasing seamless integration of health tech and blockchain.",
+    ],
+  },
+  {
+    title: "House",
+    github: "https://github.com/jovyzabel/ng-homes",
+    description: [
+      "A simple house listing app.",
+      "Built with Angular 18",
+      "Usage of standalone components",
+    ],
+  }, 
+  {
+    title: "Pokémon search app",
+    github: "https://github.com/jovyzabel/Knights-pokemon-App",
+    description: [
+      "A pokémon search app.",
+      "Built with Js from scratch",
+      "Usage of fetch API",
+    ],
+  },
+];
+/*
+export const awards = [
+  {
+    name: "IEEE YESIST12 Hackathon",
+    issuer: "IEEE",
+    date: "Sep 2022",
+    type: "International",
+    position: "Second Place",
+  },
+  {
+    name: "Prodigi Cognizant Hackathon",
+    issuer: "Cognizant",
+    date: "Feb 2023",
+    type: "National",
+    position: "Second Runner-up",
+  },
+  {
+    name: "Cisco Thingqbator Hackathon",
+    issuer: "Cisco",
+    date: "Jan 2023",
+    type: "National",
+    position: "First Runner-up",
+  },
+  {
+    name: "Innovators Day",
+    issuer: "Sri Manakula Vinayagar Engineering College, Pondicherry",
+    date: "Sep 2022",
+    type: "National",
+    position: "First Prize",
+  },
+  {
+    name: "KG Hackfest'22",
+    issuer: "KGiSL Institute of Technology, Coimbatore",
+    date: "Sep 2022",
+    type: "National",
+    position: "Second Prize",
+  },
+  {
+    name: "Innohacks'22",
+    issuer: "Innogeeks, KIET Group of Institutions, New Delhi",
+    date: "May 2022",
+    type: "National",
+    position: "Second Runner-up",
+  },
+  {
+    name: "Hack @ SKCET",
+    issuer: "Hackclub SKCET, SKCET, Coimbatore",
+    date: "Feb 2022",
+    type: "National",
+    position: "Most Impactful Hack",
+  },
+];
+*/
+export const awards = [
+  {
+    name: "FreeCodeCamp Responsive Web Design",
+    issuer: "FreeCodeCamp",
+    date: "Mars 2025",
+    type: "International",
+    position: "*",
+  },
+  {
+    name: "FreeCodeCamp Algorithms and Data Structures",
+    issuer: "FreeCodeCamp",
+    date: "Mars 2025",
+    type: "International",
+    position: "*",
+  },
+]
